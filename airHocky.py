8c0a181718cc28c9591b2d10e7e54ef76fa15ad7
SoLiare <rawissara.r@ku.th>
2017-10-03 21:24:06 +0700

    Add board

--- airHocky.py
+++ airHocky.py
@@ -1,7 +1,7 @@
 import arcade
 
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 700
 
 class AHwindow(arcade.Window):
     def __init__(self, width, height):
@@ -9,6 +9,12 @@ class AHwindow(arcade.Window):
 
         arcade.set_background_color(arcade.color.BLACK)
 
+        self.board_sprite = arcade.Sprite('board.png')
+        self.board_sprite.set_position(width // 2, height // 2)
+
+    def on_draw(self):
+        arcade.start_render()
+        self.board_sprite.draw()
 
 def main():
     window = AHwindow(SCREEN_WIDTH, SCREEN_HEIGHT)
