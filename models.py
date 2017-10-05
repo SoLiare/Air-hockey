81823414de755d52b513a8bef2a5663a4c0747c2
SoLiare <rawissara.r@ku.th>
2017-10-05 23:17:34 +0700

    Puck bounces when hits egde and players

--- models.py
+++ models.py
@@ -12,10 +12,34 @@ class Puck:
         self.x = x
         self.y = y
 
+    def direction(self, keyX, keyY):
+        if keyX==0 and keyY==0:
+            self.keyX = 0
+            self.keyY = 0
+        else:
+            if self.x==keyX:
+                self.keyX = 0
+            elif self.x > keyX:
+                self.keyX = 5
+            else:
+                self.keyX = -5
+            if self.y==keyY:
+                self.keyY = 0
+            elif self.y > keyY:
+                self.keyY = 5
+            else:
+                self.keyY = -5
+
     def update(self, delta):
-        if self.x > self.world.width:
-            self.x = 0
-        self.x+=5
+        if self.x+37.5 > self.world.width or self.x-37.5 < 0:
+            self.keyX = self.keyX*-1
+        if self.y-37.5 < 83.5 or self.y+37.5 > 533+83.5:
+            self.keyY = self.keyY*-1
+        self.x+=self.keyX
+        self.y+=self.keyY
+
+    def hit(self, other, hit_size):
+        return (abs(self.x-other.x)<=hit_size) and (abs(self.y-other.y)<=hit_size) 
 
 
 class Player1:
@@ -32,11 +56,11 @@ class Player1:
         if self.key!=0:
             if self.key=='w' and self.y < 533+83.5-50:
                 self.y+=5
-            elif self.key=='s' and self.y > 83.5+50:
+            if self.key=='s' and self.y > 83.5+50:
                 self.y-=5
-            elif self.key=='a' and self.x > 50:
+            if self.key=='a' and self.x > 50:
                 self.x-=5
-            elif self.key=='d' and self.x < self.world.width//2-50:
+            if self.key=='d' and self.x < self.world.width//2-50:
                 self.x+=5
 
 
@@ -54,12 +78,14 @@ class Player2:
         if self.key!=0:
             if self.key=='up' and self.y < 533+83.5-50:
                 self.y+=5
-            elif self.key=='down' and self.y > 83.5+50:
+            if self.key=='down' and self.y > 83.5+50:
                 self.y-=5
-            elif self.key=='left' and self.x > self.world.width//2+50:
+            if self.key=='left' and self.x > self.world.width//2+50:
                 self.x-=5
-            elif self.key=='right' and self.x < self.world.width-50:
+            if self.key=='right' and self.x < self.world.width-50:
                 self.x+=5
+
+START = 0
 
 class World:
     def __init__(self, width, height):
@@ -72,30 +98,40 @@ class World:
         self.player2 = Player2(self, width-100, height // 2)
 
     def update(self, delta):
-        self.puck.update(delta)
+        global START
+        if START==0:
+            self.puck.direction(0,0)
         self.player1.update(delta)
         self.player2.update(delta)
+        self.puck.update(delta)
+        if self.puck.hit(self.player1, 75):
+            self.puck.direction(self.player1.x, self.player1.y)
+        if self.puck.hit(self.player2, 75):
+            self.puck.direction(self.player2.x, self.player2.y)
+
+        START+=1
+
 
     def on_key_press(self, key, key_modifiers):
         if key==arcade.key.W:
             self.player1.direction('w')
-        elif key==arcade.key.S:
+        if key==arcade.key.S:
             self.player1.direction('s')
-        elif key==arcade.key.A:
+        if key==arcade.key.A:
             self.player1.direction('a')
-        elif key==arcade.key.D:
+        if key==arcade.key.D:
             self.player1.direction('d')
         if key==arcade.key.UP:
             self.player2.direction('up')
-        elif key==arcade.key.DOWN:
+        if key==arcade.key.DOWN:
             self.player2.direction('down')
-        elif key==arcade.key.LEFT:
+        if key==arcade.key.LEFT:
             self.player2.direction('left')
-        elif key==arcade.key.RIGHT:
+        if key==arcade.key.RIGHT:
             self.player2.direction('right')
 
     def on_key_release(self, key, key_modifiers):
         if key==arcade.key.W or key==arcade.key.S or key==arcade.key.A or key==arcade.key.D:
             self.player1.direction('0')
-        elif key==arcade.key.UP or key==arcade.key.DOWN or key==arcade.key.RIGHT or key==arcade.key.LEFT:
+        if key==arcade.key.UP or key==arcade.key.DOWN or key==arcade.key.RIGHT or key==arcade.key.LEFT:
             self.player2.direction('0')
