dad3fad096061740d799bb4c3d3868f51ecd529b
SoLiare <rawissara.r@ku.th>
2017-10-06 11:14:04 +0700

    Resize puck and players

--- models.py
+++ models.py
@@ -1,5 +1,10 @@
 import arcade.key
 
+PUCK = 50
+PLAYER = 75
+
+START = 0
+
 class Board:
     def __init__(self, x, y):
         self.x = x
@@ -11,6 +16,10 @@ class Puck:
         self.world = world
         self.x = x
         self.y = y
+
+    def start_point(self):
+        self.x = self.world.width//2
+        self.y = self.world.height//2
 
     def direction(self, keyX, keyY):
         if keyX==0 and keyY==0:
@@ -31,15 +40,19 @@ class Puck:
                 self.keyY = -5
 
     def update(self, delta):
-        if self.x+37.5 > self.world.width or self.x-37.5 < 0:
-            self.keyX = self.keyX*-1
-        if self.y-37.5 < 83.5 or self.y+37.5 > 533+83.5:
-            self.keyY = self.keyY*-1
+        if not self.goal():
+            if self.x+PUCK//2 > self.world.width or self.x-PUCK//2 < 0:
+                self.keyX = self.keyX*-1
+            if self.y-PUCK//2 < 83.5 or self.y+PUCK//2 > 533+83.5:
+                self.keyY = self.keyY*-1
         self.x+=self.keyX
         self.y+=self.keyY
 
     def hit(self, other, hit_size):
-        return (abs(self.x-other.x)<=hit_size) and (abs(self.y-other.y)<=hit_size) 
+        return (abs(self.x-other.x)<=hit_size) and (abs(self.y-other.y)<=hit_size)
+
+    def goal(self):
+        return (self.x-37.5 > 800 or self.x+37.5 < 0) and (self.y-37.5 > 216.75 and self.y+37.5 < 483.25)
 
 
 class Player1:
@@ -49,18 +62,22 @@ class Player1:
         self.y = y
         self.key = '0'
 
+    def start_point(self):
+        self.x = 100
+        self.y = self.world.height//2
+
     def direction(self, key):
         self.key = key
 
     def update(self, delta):
         if self.key!=0:
-            if self.key=='w' and self.y < 533+83.5-50:
+            if self.key=='w' and self.y < 533+83.5-PLAYER//2:
                 self.y+=5
-            if self.key=='s' and self.y > 83.5+50:
+            if self.key=='s' and self.y > 83.5+PLAYER//2:
                 self.y-=5
-            if self.key=='a' and self.x > 50:
+            if self.key=='a' and self.x > PLAYER//2:
                 self.x-=5
-            if self.key=='d' and self.x < self.world.width//2-50:
+            if self.key=='d' and self.x < self.world.width//2-PLAYER//2:
                 self.x+=5
 
 
@@ -71,21 +88,24 @@ class Player2:
         self.y = y
         self.key = '0'
 
+    def start_point(self):
+        self.x = self.world.width-100
+        self.y = self.world.height//2
+
     def direction(self, key):
         self.key = key
 
     def update(self, delta):
         if self.key!=0:
-            if self.key=='up' and self.y < 533+83.5-50:
+            if self.key=='up' and self.y < 533+83.5-PLAYER//2:
                 self.y+=5
-            if self.key=='down' and self.y > 83.5+50:
+            if self.key=='down' and self.y > 83.5+PLAYER//2:
                 self.y-=5
-            if self.key=='left' and self.x > self.world.width//2+50:
+            if self.key=='left' and self.x > self.world.width//2+PLAYER//2:
                 self.x-=5
-            if self.key=='right' and self.x < self.world.width-50:
+            if self.key=='right' and self.x < self.world.width-PLAYER//2:
                 self.x+=5
 
-START = 0
 
 class World:
     def __init__(self, width, height):
@@ -104,13 +124,17 @@ class World:
         self.player1.update(delta)
         self.player2.update(delta)
         self.puck.update(delta)
-        if self.puck.hit(self.player1, 75):
+        if self.puck.hit(self.player1, PLAYER//2+PUCK//2):
             self.puck.direction(self.player1.x, self.player1.y)
-        if self.puck.hit(self.player2, 75):
+        if self.puck.hit(self.player2, PLAYER//2+PUCK//2):
             self.puck.direction(self.player2.x, self.player2.y)
-
-        START+=1
-
+        if self.puck.goal():
+            START = 0
+            self.puck.start_point()
+            self.player1.start_point()
+            self.player2.start_point()
+        else:
+            START+=1
 
     def on_key_press(self, key, key_modifiers):
         if key==arcade.key.W:
